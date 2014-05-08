b6c1846f17793af6f474d278d53b5968196f7279
jay16 <jay_li@intfocus.com>
2014-05-08 10:17:44 +0800

    sqlite db文件需要有被读写的权限

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,11 @@
 require "date"
+
 configure do
   db_name =[ENV["APP_NAME"], ENV["RACK_ENV"]].join("_")
   DataMapper::setup(:default, "sqlite3://#{ENV['APP_ROOT_PATH']}/db/#{db_name}.db")
+
+  #启动后保证db文件有被读写权限
+  system("chmod 777 #{ENV['APP_ROOT_PATH']}/db/*")
 
   # 加载所有models
   Dir.glob("#{ENV['APP_ROOT_PATH']}/app/models/*.rb").each { |file| require file }
@@ -9,23 +13,5 @@ configure do
   # 自动迁移数据库
   DataMapper.finalize.auto_upgrade!
   #DataMapper.finalize.auto_migrate!
-
-=begin
-  IO.readlines("./tea.txt").each do |line|
-    array = line.split
-    hash = {
-      :name => array[0],
-      :image => array[1],
-      :from => array[2],
-      :price => array[3],
-      :unit1 => array[4],
-      :unit2 => array[5],
-      :weight => array[6],
-      :unit3 => array[7],
-      :desc => array[8]
-    }
-    Tea.create(hash)
-  end
-=end
 end
 
