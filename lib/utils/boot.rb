23fe688622080933317bf6fe331eb022b5ecef41
jay16 <jay_li@intfocus.com>
2014-10-09 00:52:25 +0800

    split array with partition

--- lib/utils/boot.rb
+++ lib/utils/boot.rb
@@ -2,23 +2,18 @@
 module Utils
   module Boot
     def recursion_require(dir_path, regexp, base_path = ENV["APP_ROOT_PATH"], sort_rules = [])
-      _temp_files, _temp_dirs = [], []
-      Dir.entries("%s/%s" % [base_path, dir_path]).sort.each do |dir_name|
-        next if %w[. ..].include?(dir_name)
-
-        path_name = "%s/%s/%s" % [base_path, dir_path, dir_name]
-        if File.file?(path_name) and dir_name =~ /\.rb$/
-          dir_name.scan(regexp).empty? ? (warn "warning not match #{regexp.inspect} - #{dir_name}") : _temp_files.push(path_name)
-        elsif File.directory?(path_name)
-          _temp_dirs.push(dir_name)
-        end
-      end
+     _partition = Dir.entries("%s/%s" % [base_path, dir_path])
+       .reject { |dir_name| %w[. ..].include?(dir_name) }
+       .partition { |dir_name| File.file?("%s/%s/%s" % [base_path, dir_path, dir_name]) }
+      _temp_files, _temp_dirs = *_partition
 
       if not sort_rules.empty?
         _temp_files = sort_by_rules(_temp_files, sort_rules) 
       end
       _temp_files.each do |file|
-        require file
+        file_path = "%s/%s/%s" % [base_path, dir_path, file]
+        warn_info =  "warning not match %s - %s" % [regexp.inspect, file_path]
+        file.scan(regexp).empty? ? (warn warn_info) : (require file_path)
       end if not _temp_files.empty?
 
       _temp_dirs.each do |dir_name|
